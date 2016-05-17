fd8723ff490b029ae3031cf7883120588014f5d1
FrederikV78 <frederik.vanstraelen@student.ucll.be>
2016-05-17 18:09:44 +0200

    Details listing view added

--- App-project/ShowItemsFromKeyword.xaml.cs
+++ App-project/ShowItemsFromKeyword.xaml.cs
@@ -15,6 +15,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using App_project;
+using Windows.System;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
@@ -25,9 +27,15 @@ namespace App_project
     /// </summary>
     public sealed partial class ShowItemsFromKeyword : Page
     {
+
+        List<RSSItem> listItems = new List<RSSItem>();
+        string keyword;
+
         public ShowItemsFromKeyword()
         {
             this.InitializeComponent();
+            keyword = (string)ApplicationData.Current.LocalSettings.Values["keyword"];
+            listItems = GetItemsList(keyword);
         }
 
         /// <summary>
@@ -37,13 +45,13 @@ namespace App_project
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            string keyword = "nokeywordavailable";
+            keyword = "nokeywordavailable";
             keyword = (string)ApplicationData.Current.LocalSettings.Values["keyword"];
 
             Debug.WriteLine("*** *** *** keyword: {0}  SELECTED",keyword);
-
-            listbox1.ItemsSource = GetItemsList(keyword);
-
+            
+            listItems = GetItemsList(keyword);
+            listbox1.ItemsSource = listItems;
         }
 
         private void AppBarButton_Click(object sender, RoutedEventArgs e) //BACK BUTTON
@@ -51,9 +59,13 @@ namespace App_project
             this.Frame.Navigate(typeof(ShowKeywords));
         }
 
-        private void AppBarButton1_Click(object sender, RoutedEventArgs e) //DETAILS BUTTON
+        private async void AppBarButton1_Click(object sender, RoutedEventArgs e) //DETAILS BUTTON
         {
+            int selectedIndex = listbox1.SelectedIndex;
+            //RSSItem item = new RSSItem;
+            var selectedItemLink = listItems[selectedIndex].Link;
             
+            await Launcher.LaunchUriAsync(selectedItemLink);
 
 
         }
@@ -69,30 +81,39 @@ namespace App_project
         // // // // // //
         //// METHODS ////
         // // // // // //
-        public List<string> GetItemsList(string keyword)
+        public List<RSSItem> GetItemsList(string keyword)
         {
             //(ItemId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,KId INTEGER NOT NULL, Keyword varchar(30), Title varchar(30) UNIQUE, Link varchar(100), Description varchar(1024), PubDate varchar(30))
             try
-            {
-                string query = "SELECT ItemId,Title,Link, Description, PubDate FROM Items WHERE Keyword=@keyword ORDER BY ItemId DESC;";
+            {       //ItemId,Title,Link, Description, PubDate
+                string query = "SELECT * FROM Items WHERE Keyword=@keyword ORDER BY ItemId DESC;";
                 using (SQLiteConnection conn = new SQLiteConnection("Keywords.db"))
                 {
                     using (SQLitePCL.ISQLiteStatement statement = conn.Prepare(query))
                     {
-                        List<string> LijstItems = new List<string>();
+                        List<RSSItem> LijstItems = new List<RSSItem>();
+                        RSSItem item;
 
                         int i = 0;
                         Debug.WriteLine("   *** START db items ***   ");
                         statement.Bind("@keyword", keyword);
                         while (statement.Step() == SQLiteResult.ROW)
                         {
+                            item = new RSSItem();
                             i++;
-                            string title = (string)statement[1];
-                            LijstItems.Add(title);
+                            item.UniqueId = statement[0].ToString();
+                            item.Title = (string)statement[3];
+                            item.Link = new Uri(statement[4].ToString(), UriKind.Absolute);
+                            item.Description = (string)statement[5];
+                            item.PubDate = DateTime.Parse(statement[6].ToString());
+
+                            LijstItems.Add(item);
                         }
                         if (i == 0)
                         {
-                            LijstItems.Add("Nothing to show!");
+                            item = new RSSItem();
+                            item.Title = "Nothing to show!";
+                            LijstItems.Add(item);
                         }
                         else
                         {
